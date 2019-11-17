1c93d484ecb13ae0cb2caf43a79eef9fa60b3738
it-Hooligan <57491204+it-Hooligan@users.noreply.github.com>
2019-11-17 20:34:40 +0200

    js учусь работать

---
+++ js/script.js
@@ -0,0 +1,8 @@
+let name= prompt('Тебя как зовут?', );
+alert(`Тебя зовут ${name}`);
+let friend=prompt('А как зовут твоего другa?', );
+friend=prompt(` о точно это ${friend} Рублевский?`);
+if(friend=='да','Да'){alert('Как хорошо, что я не забыл))')}else{
+	let question=prompt(`Тю еб твою мать а как его зовут?`,);
+	alert(`ОТ СПАСИБО теперь буду знать что его зовут ${question}`);
+}
